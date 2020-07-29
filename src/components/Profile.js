a1facfb39af21b45926944b0bec84574add3947d
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-07-29 12:56:02 +0200

    add new view

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import { Container, Header, Icon, Image, Menu, Segment, Sidebar, Button } from 'semantic-ui-react'
+
 
 export default class Profile extends Component{
     render(){
         return(
-            Profile
+            <>
+                <Header as='h2' icon textAlign='center'>
+                    <i aria-hidden="true" class="users icon left"></i>
+                    <Header.Content>Projects</Header.Content>
+                </Header>
+                <Header as='h2' icon textAlign='center'>
+                    <Icon name='settings' circular />
+                    <Header.Content>Tasks</Header.Content>
+                </Header>
+                <Header as='h2' icon textAlign='center'>
+                    <Icon name='plug' circular />
+                    <Header.Content>Processes</Header.Content>
+                </Header>
+            </>
         )
     }
 }
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -12,18 +12,7 @@ export default class Projects extends Component{
             <>
             <Button> Add project </Button><br></br><br></br>
             <ProjectCard/>
-            <Header as='h2' icon textAlign='center'>
-                <i aria-hidden="true" class="users icon left"></i>
-                <Header.Content>Projects</Header.Content>
-            </Header>
-            <Header as='h2' icon textAlign='center'>
-                <Icon name='settings' circular />
-                <Header.Content>Tasks</Header.Content>
-            </Header>
-            <Header as='h2' icon textAlign='center'>
-                <Icon name='plug' circular />
-                <Header.Content>Processes</Header.Content>
-            </Header>
+            
             </>
         )
     }
