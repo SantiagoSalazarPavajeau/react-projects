fc9febca0e9991f3d29f7f1016ec4f314b48871a
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-07-29 16:07:38 +0200

    add initial project state in reducer

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -1,3 +1,21 @@
-export default function projectsReducer(){
-    
+let date = new Date()
+let year = date.getFullYear()
+let month = date.getMonth()
+let day = date.getDate()
+let started = `Started ${month} ${day}, ${year}`
+
+export default function projectsReducer(
+    state = {
+        projects: [{
+            title: 'React-Redux', 
+            started: started,
+            description: 'Build with project requirements'
+        }]
+    }, 
+    action){
+        switch(action.type){
+            default:
+                return state
+        }
+
 }
