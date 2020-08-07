ba87e46d554131a766cd35303c4bd45f1888d4a8
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-08-07 08:31:55 -0400

    add people dropdown to assign tasks

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -6,38 +6,26 @@ const friendOptions = [
       key: 'Jenny Hess',
       text: 'Jenny Hess',
       value: 'Jenny Hess',
-      image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
+      image: { avatar: true, src: 'https://api.adorable.io/avatars/77/jenny@adorable.io.png' },
     },
     {
       key: 'Elliot Fu',
       text: 'Elliot Fu',
       value: 'Elliot Fu',
-      image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
+      image: { avatar: true, src: 'https://api.adorable.io/avatars/77/elliot@adorable.io.png' },
     },
     {
       key: 'Stevie Feliciano',
       text: 'Stevie Feliciano',
       value: 'Stevie Feliciano',
-      image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
+      image: { avatar: true, src: 'https://api.adorable.io/avatars/77/stevie@adorable.io.png' },
     },
     {
       key: 'Christian',
       text: 'Christian',
       value: 'Christian',
-      image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
-    },
-    {
-      key: 'Matt',
-      text: 'Matt',
-      value: 'Matt',
-      image: { avatar: true, src: '/images/avatar/small/matt.jpg' },
-    },
-    {
-      key: 'Justen Kitsune',
-      text: 'Justen Kitsune',
-      value: 'Justen Kitsune',
-      image: { avatar: true, src: '/images/avatar/small/justen.jpg' },
-    },
+      image: { avatar: true, src: 'https://api.adorable.io/avatars/77/christian@adorable.io.png' },
+    }
   ]
 
 const Task = ({description, completed}) => {
@@ -47,20 +35,30 @@ const Task = ({description, completed}) => {
     
     const renderInProgress = () => {
         return (
+            <>
             <br></br>
             <Button onClick={handleComplete} icon="check circle"></Button>  <Input value={description} placeholder={description}></Input> 
             <Dropdown placeholder='People' search selection options={friendOptions} />
             <br></br>
+            </>
+
         )
     }
      const renderCompleted = () => {
-         
+        return (
+            <>
+            <br></br>
+            <Button onClick={handleComplete} icon="check circle"></Button>  <Input value={description} placeholder={description}></Input> 
+            <Dropdown placeholder='People' search selection options={friendOptions} />
+            <br></br>
+            </>
+        )     
      }
     
     return  (
-            <div>
-                {completed ? renderCompleted : renderInProgress}
-            </div>
+            <>
+            {completed ? renderCompleted() : renderInProgress()}
+            </>
     )
 }
 
