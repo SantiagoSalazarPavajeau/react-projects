e8593f689ada33fe2498912da6caacc3830bd736
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-08-10 22:51:58 -0400

    update tasks modal style

--- src/components/tasks/TasksModal.js
+++ src/components/tasks/TasksModal.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Header, Image, Modal, Form, Button, Divider } from 'semantic-ui-react'
+import { Header, Image, Modal, Form, Button, Divider, Grid } from 'semantic-ui-react'
 import {withRouter} from 'react-router-dom';
 import Task from './Task'
 
@@ -70,10 +70,22 @@ class TasksModal extends Component{
     renderDescription = () => {
         return (
             <>
+                <Grid columns={3}>
+                <Grid.Row>
+                    <Grid.Column>
+                    <Button basic secondary content="Edit Project" labelPosition='right' icon="pencil" onClick={e => this.startEdit(e)} ></Button>                               
+                    </Grid.Column>
+                    <Grid.Column>
+                    </Grid.Column>
+                    <Grid.Column>
+                    <Button basic content="Exit Project Page"  onClick={this.props.history.goBack} secondary ></Button>
+                    </Grid.Column>
+                    </Grid.Row>
+                </Grid> 
                 <Header>{this.state.title}</Header>
-                    <p>{this.state.description}</p>
-                    <Button content="Edit" labelPosition='right' icon="pencil" onClick={e => this.startEdit(e)} ></Button>                               
-                    <Button content="Exit" labelPosition='right' icon="close" onClick={this.props.history.goBack} ></Button>      
+                <p>{this.state.description}</p>
+
+                      
             </>          
         )
     }
@@ -95,7 +107,7 @@ class TasksModal extends Component{
                                     <Modal.Description>
                                     {this.state.edit ?  this.renderEdit() : this.renderDescription()}
                                     <Divider horizontal>Tasks</Divider>
-                                    <Button onClick={this.handleAddTask}>Add Task</Button>
+                                    <Button basic secondary onClick={this.handleAddTask}>Add Task</Button>
                                     <br></br>
                                     {this.renderTasks()}
                                     {/* {console.log(this.props.tasks.filter(task => task.projectId === this.state.id))} */}
